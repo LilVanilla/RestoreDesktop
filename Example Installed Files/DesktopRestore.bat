d0d707d8adaee0c34f373b4e2386bb53f488ab95
Vanilla <57304109+LilVanilla@users.noreply.github.com>
2024-11-15 12:31:24 -0500

    Update DesktopRestore.bat
    changed file path

--- Example Installed Files/DesktopRestore.bat
+++ Example Installed Files/DesktopRestore.bat
@@ -1,3 +1,3 @@
 Taskkill /IM "explorer.exe" /F
-REG IMPORT C:\Users\%USERNAME%\Documents\IconSafe\Desktopsave.txt
+REG IMPORT C:\Users\%USERNAME%\Documents\RestoreDesktop\Desktopsave.txt
 start explorer.exe
